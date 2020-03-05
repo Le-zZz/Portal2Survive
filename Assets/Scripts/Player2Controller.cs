151b5c9cfac7279c9ce50deb1479c117d8554d21
Bryan <bryancrim@gmail.com>
2020-03-05 16:37:45 +0100

    PlayerController & WallPrefabs added

---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    [SerializeField] float movementX = 5;
+    [SerializeField] float movementY = 5;
+    [SerializeField] float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementX = Input.GetAxis("Horizontal2");
+        movementY = Input.GetAxis("Vertical2");
+        
+        body.velocity = new Vector2(movementX * speed, movementY * speed);
+    }
+}
+
