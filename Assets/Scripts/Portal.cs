aebf9789e7e7d35114bd939e2d5cafb98700b5a4
Bryan <bryancrim@gmail.com>
2020-03-07 15:42:03 +0100

    Portal & PortalMaker Scripts added

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private float time = 4;
+    
+
     void Update()
     {
-        
+        Destroy(gameObject, time);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/PortalMaker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMaker : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject portalPrefab;
+    
+    //public AudioSource bulletSound;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Shot();
+        }
+    }
+
+    public void Shot()
+    {
+        GameObject bullet = Instantiate(portalPrefab, spawnPoint.position, spawnPoint.rotation); 
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>(); 
+        
+    }
+}
