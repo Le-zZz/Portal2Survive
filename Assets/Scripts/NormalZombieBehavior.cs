03f1cd232c0970ad4b41444ef8be251b63e9d4a6
L <eleonora.dups@gmail.com>
2020-04-03 15:26:36 +0200

    changed layer players & started script for enemy behavior

---
+++ Assets/Scripts/NormalZombieBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalZombieBehavior : MonoBehaviour
+{
+//    var distance = Vector3.Distance(transform.position, player.transform.position)
+
+//if(distance< 5){
+//rigidbody.velocity = (player.transform.position - transform.position).normalize* speed;
+//}
+void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
