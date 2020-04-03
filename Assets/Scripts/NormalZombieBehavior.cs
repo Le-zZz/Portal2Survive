9600a47b1e29e9b3227f5dad67b0b9c88e9896f2
Bryan <bryancrim@gmail.com>
2020-04-03 18:55:51 +0200

    Fix Zombie scripts

--- Assets/Scripts/NormalZombieBehavior.cs
+++ Assets/Scripts/NormalZombieBehavior.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class NormalZombieBehavior : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
     private Rigidbody2D body;
 
     private int playerDistance = 10;
     private float speed = 1f;
+
+    private bool followPlayer1 = false;
+    private bool followPlayer2 = false;
 
     void Start()
     {
@@ -16,18 +20,35 @@ public class NormalZombieBehavior : MonoBehaviour
     }
     void Update()
     {
-        var direction = Vector3.Distance(transform.position, player.transform.position);
-
-        if(direction < playerDistance)
+        if (followPlayer1)
         {
-            body.velocity = (player.transform.position - transform.position).normalized * speed;
+            var direction = Vector3.Distance(transform.position, player1.transform.position);
+
+            if (direction < playerDistance)
+            {
+                body.velocity = (player1.transform.position - transform.position).normalized * speed;
+            }
+        }
+        if (followPlayer2)
+        {
+            var direction = Vector3.Distance(transform.position, player2.transform.position);
+
+            if (direction < playerDistance)
+            {
+                body.velocity = (player2.transform.position - transform.position).normalized * speed;
+            }
         }
     }
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
-    //    {
 
-    //    }
-    //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "leftSide")
+        {
+            followPlayer1 = true;
+        }
+        if (collision.gameObject.tag == "rightSide")
+        {
+            followPlayer2 = true;
+        }
+    }
 }
